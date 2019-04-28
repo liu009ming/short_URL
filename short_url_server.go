e68564de9b159eebb644471949229e35f1082128
liu009ming <31750624+liu009ming@users.noreply.github.com>
2019-04-28 19:45:30 +0800

    Update short_url_server.go

--- short_url_server.go
+++ short_url_server.go
@@ -3,12 +3,23 @@ package main
 import "fmt"
 import "log"
 import "net/http"
+import "./db"
+import "database/sql"
+import _"github.com/go-sql-driver/mysql"
 
+type DbWorker struct {
+	Dsn string 
+}
+var DB *sql.DB
 func main() {
+	DB=db.Connect()
 	http.HandleFunc("/",hanler)
 	log.Fatal(http.ListenAndServe("localhost:8000",nil))
 }
 
 func hanler(w http.ResponseWriter,r *http.Request) {
 	fmt.Fprintf(w,"URL.Path = %q\n",r.URL.Path)
+	shortUrl:=db.GetShortUrl(DB,r.URL.Path)
+	fmt.Fprintf(w,"shortUrl=http://liu123/%d",shortUrl)
 }
+
