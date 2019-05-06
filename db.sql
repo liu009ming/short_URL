65266048cccdec6009ee44d6ead88d9710c1c167
liu009ming <31750624+liu009ming@users.noreply.github.com>
2019-05-06 16:46:24 +0800

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,5 +1,5 @@
 create table short_url(
-	‘shortUrl’ bigint(64) not null auto_increment,
-	‘longUrl’ varchar(10240) not null,
-	primary key (‘shortUrl’)
+	shortUrl bigint(64) not null auto_increment,
+	longUrl varchar(10240) not null,
+	primary key (shortUrl)
 )engine=InnoDB default charset=utf8;
